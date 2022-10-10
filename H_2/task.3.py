294e1c477a3b8c8c5aa6f650179333612252b3fd
Solin <svetaslina96@gmail.com>
2022-10-10 20:48:36 +0300

    НТ3

--- H_2/task.3.py
+++ H_2/task.3.py
@@ -1,8 +1,8 @@
-#3.Напишите скрипт, который принимает <number> от пользователя и выводит сумму первых <number>
+# 3.Напишите скрипт, который принимает <number> от пользователя и выводит сумму первых <number>
 # положительных целых чисел.
 
 numbers = int(input("Введите число: "))
-Evrika = 0
+evrika = 0
 for i in range(numbers+1):
-    Evrika = Evrika + i
-print(Evrika)
+    evrika+= i
+print(evrika)
